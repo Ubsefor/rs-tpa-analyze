1b045e2ea2c98f8607645a1b846c8314adc2606a
Ubsefor <letzdropthebas@gmail.com>
2021-03-09 13:30:18 +0300

    Update unpacking script

--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-unzip NC*
+unzip NC_000001.11\ Homo\ sapiens\ chromosome\ 1,\ GRCh38.p13\ Primary\ Assembly.fasta.zip NC_000001.11\ Homo\ sapiens\ chromosome\ 1,\ GRCh38.p13\ Primary\ Assembly.fasta
+unzip NC_000015.10_Homo_sapiens_chromosome_15,GRCh38.p13_Primary_Assembly.fasta.zip NC_000015.10_Homo_sapiens_chromosome_15,GRCh38.p13_Primary_Assembly.fasta  
