0ded032bcc5e375827ee007ba43f83b49c1cfd9b
Ubsefor <letzdropthebas@gmail.com>
2021-03-09 12:20:58 +0300

    Add base project files

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use percentage::Percentage;
+use regex::Regex;
+
+fn usage() {
+    println!("Usage: rs-tpa-analyze <genome file path>\nNote, that file must not contain header!");
+}
+
+fn version(){
+    println!("version: 0.1.0");
+}
+
+pub fn count_genes(genome : String, gene : char) -> usize {
+    return genome.matches(gene).count();
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            usage();
+            return;
+        },
+
+        2 => {
+            if  &args[1] == "-h" {
+                usage();
+                return;
+            }
+            if &args[1] == "-v"{
+                version();
+                return;
+            }
+            let filepath = &args[1];
+            if !(Path::new(filepath).is_file()){
+                panic!("Error: couldn't read file at specified filepath!");
+            }
+            let contents = fs::read_to_string(filepath)
+                .expect("Something went wrong reading the file");
+            if !(contents.chars().nth(0).unwrap() == 'A' 
+                    || contents.chars().nth(0).unwrap() == 'C'
+                    || contents.chars().nth(0).unwrap() == 'G' 
+                    || contents.chars().nth(0).unwrap() == 'T'
+                    || contents.chars().nth(0).unwrap() == 'N') {
+                panic!("Error: file seems to contain a header or not a raw genome!")
+            }
+            println!("Parsing genome in file: {}", filepath);
+            let mut size : usize = contents.len();
+            if contents.chars().last().unwrap() == '\n' {
+                size = contents.len() - 1;
+            }
+            println!("Total genome size: {}", size);
+            
+            return ;
+        },
+        
+        _ => {
+            usage();
+            return;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_counts(){
+        assert_eq!(count_genes("NNN".to_string(), 'N'), 3);
+    }
+
+    #[test]
+    fn test_cgaps(){
+        assert_eq!(count_gaps("ANNNNNNNNNN".to_string()), 1);
+        assert_eq!(count_gaps("AN".to_string()), 0);
+        assert_eq!(count_gaps("A".to_string()), 0);
+        assert_eq!(count_gaps("ANANA".to_string()), 0);
+        assert_eq!(count_gaps("ANNNNNNNNNNANA".to_string()), 1);
+        assert_eq!(count_gaps("ANNNNNNNNNNNNNNNNNNNNNNNNNANA".to_string()), 1);
+    }
+    
+}
+
